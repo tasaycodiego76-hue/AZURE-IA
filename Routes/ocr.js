a77b170055b605ff2ceb849f7bbbc79a1ecdc2dc
Diego Tasayco <tasaycodiego76@gmail.com>
2026-05-26 10:28:52 -0500

    Mejora de controlador OCR

--- Routes/ocr.js
+++ Routes/ocr.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const OCRController = require('../Controllers/OCRController')
+const OCRController = require('../Controllers/OcrController')
 
 // POST /api/ocr
 router.post('/', OCRController.leerTextoImagen)
